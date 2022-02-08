f811f54a09871355241f394af83878fbbc2a8e0d
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-02-08 11:19:47 +0200

    Сделал

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,25 @@
 const formRef = document.querySelector(".login-form");
 
+const userData = {};
+
 const preventDefault = (event) => event.preventDefault();
 
-const alert = (event) => {
+const userInput = (event) => {
   const {
     elements: { email, password },
   } = event.currentTarget;
 
-  if(email.value ==="" || password.value ===""){
-      window.alert("Че ты тупишь... Все поля должны быть заполнены! Сечешь, матьтвою!?")
+  if (email.value === "" || password.value === "") {
+    window.alert("Все поля должны быть заполнены! Сёчешь, матьтвою!?");
+  } else {
+    userData.email = email.value;
+    userData.password = password.value;
+
+    console.log(userData)
+
+    formRef.reset()
   }
 };
 
-
-
 formRef.addEventListener("submit", preventDefault);
-formRef.addEventListener("submit", alert);
+formRef.addEventListener("submit", userInput);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const buttonRef = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const bodyBackground = document.querySelector("body")
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const backgrounColor = () => {
+  const randomColor = getRandomHexColor();
+
+  bodyBackground.style.backgroundColor = randomColor
+  colorValue.textContent = randomColor
+};
+
+buttonRef.addEventListener("click", backgrounColor);
