4f89303a0f642303757a6d90ecf3f981c4d0903d
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-02-11 12:19:08 +0200

    Начал делать задачу №10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsRef = document.querySelector("#controls");
+const inputRef = document.querySelector("input");
+
+console.log(inputRef);
+
+const click = (event) => {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  inputRef.addEventListener("input", (event) => {
+    const inputValue = event.currentTarget.value;
+    console.log(inputValue);
+  });
+
+  
+};
+
+controlsRef.addEventListener("click", click);
